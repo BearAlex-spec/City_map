95c27af4e6216913d91db1e2610898a6afdc0b64
79252 <jackstreymon@gmail.com>
2024-11-05 14:42:52 +0300

    Добавлены слова "широта" и "долгота"

--- main.py
+++ main.py
@@ -6,13 +6,15 @@ def get_coordinates(city, key):
         geocoder = OpenCageGeocode(key)
         results = geocoder.geocode(city, language = "ru")
         if results:
-            return results[0]['geometry']['lat'], results[0]['geometry']['lng']
+            lat = round(results[0]['geometry']['lat'], 2)
+            lan = round(results[0]['geometry']['lng'], 2)
+            return f"широта: {lat}, долгота: {lan}"
         else:
             return "Город не найден"
     except Exception as e:
         return f"Возникла ошибка: {e}"
 
 key = 'c3626e750a6d4b9fa2df917627e3a783'
-city = "London"
+city = "Люберцы"
 coordinates = get_coordinates(city, key)
 print(f"Координаты города {city}: {coordinates}")
